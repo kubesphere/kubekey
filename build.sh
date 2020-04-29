4ebbdcb9c3aee288eb7709e7a5c6aa8759ca8d7d
pixiake <guotongxue123@163.com>
2020-04-29 23:10:29 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:1.14 go build -v -o kk
+docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:1.14.2-buster go build -v -o kk
