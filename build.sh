f2db638d41407467d15a9d88aa532128fd02ab57
Ray Zhou <ray@yunify.com>
2020-05-18 09:53:26 +0800

    Update build.sh to includee a proxy option

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
 
-docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:1.14.2-buster go build -v -o kk
-
-# Using the most trusted Go module proxy in China mainland
-# docker run --rm -e GO111MODULE=on -e GOPROXY=https://goproxy.cn -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:1.14.2-buster go build -v -o kk
-
+if [ -n "$1" ]; then 
+    if [ "$1" == "-p" ] || [ "$1" == "--proxy" ]; then
+        # Using the most trusted Go module proxy in China
+        docker run --rm -e GO111MODULE=on -e GOPROXY=https://goproxy.cn -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:1.14.2-buster go build -v -o kk
+    else
+        echo "The option should be '-p' or '--proxy'"
+    fi
+else
+    docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:1.14.2-buster go build -v -o kk
+fi
