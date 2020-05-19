b7c7564dd3eebd3afe3a65e0a7e69ac4fa283681
Ray Zhou <ray@yunify.com>
2020-05-19 16:17:24 +0800

    change the build.sh to output kk to the folder output

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
 if [ -n "$1" ]; then 
     if [ "$1" == "-p" ] || [ "$1" == "--proxy" ]; then
         # Using the most trusted Go module proxy in China
-        docker run --rm -e GO111MODULE=on -e GOPROXY=https://goproxy.cn -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:1.14.2-buster go build -v -o kk
+        docker run --rm -e GO111MODULE=on -e GOPROXY=https://goproxy.cn -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:1.14.2-buster go build -v -o output/kk
     else
         echo "The option should be '-p' or '--proxy'"
     fi
 else
-    docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:1.14.2-buster go build -v -o kk
+    docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:1.14.2-buster go build -v -o kk output/kk
 fi
