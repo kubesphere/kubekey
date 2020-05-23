856637c3dd3f262375898b1540e681eb9a236dcd
pixiake <guotongxue123@163.com>
2020-05-23 13:23:40 +0800

    add version compare

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -12,7 +12,10 @@ import (
 )
 
 const (
-	VERSION = "KubeKey Version v0.0.1\nKubernetes Version v1.17.4"
+	VERSION      = "KubeKey Version v0.0.1\n"
+	VersionBig   = 1
+	VersionSmall = 2
+	VersionEqual = 0
 )
 
 func InitLogger(verbose bool) *log.Logger {
@@ -218,4 +221,75 @@ func LocalIP() string {
 		log.Fatalf("Failed to get Local IP: %v", err)
 	}
 	return localIp
+}
+
+// Compare Version Numbers
+func CompareVersion(versionA, versionB string) int {
+	return compareStrVer(versionA, versionB)
+}
+
+func compareStrVer(verA, verB string) int {
+
+	verStrArrA := spliteStrByNet(verA)
+	verStrArrB := spliteStrByNet(verB)
+
+	lenStrA := len(verStrArrA)
+	lenStrB := len(verStrArrB)
+
+	if lenStrA != lenStrB {
+		panic("Inconsistent version number format!")
+
+	}
+
+	return compareArrStrVers(verStrArrA, verStrArrB)
+}
+
+func spliteStrByNet(strV string) []string {
+	return strings.Split(strV, ".")
+}
+
+func compareArrStrVers(verA, verB []string) int {
+
+	for index, _ := range verA {
+
+		littleResult := compareLittleVer(verA[index], verB[index])
+
+		if littleResult != VersionEqual {
+			return littleResult
+		}
+	}
+
+	return VersionEqual
+}
+
+func compareLittleVer(verA, verB string) int {
+
+	bytesA := []byte(verA)
+	bytesB := []byte(verB)
+
+	lenA := len(bytesA)
+	lenB := len(bytesB)
+	if lenA > lenB {
+		return VersionBig
+	}
+
+	if lenA < lenB {
+		return VersionSmall
+	}
+
+	return compareByBytes(bytesA, bytesB)
+}
+
+func compareByBytes(verA, verB []byte) int {
+
+	for index, _ := range verA {
+		if verA[index] > verB[index] {
+			return VersionBig
+		}
+		if verA[index] < verB[index] {
+			return VersionSmall
+		}
+	}
+
+	return VersionEqual
 }
