c8d5ae954220b8812c4023939340c842431830f8
pixiake <guotongxue123@163.com>
2020-06-23 09:49:41 +0800

    update calico to 2.14.1

--- pkg/plugins/network/network.go
+++ pkg/plugins/network/network.go
@@ -26,6 +26,7 @@ import (
 	"github.com/kubesphere/kubekey/pkg/util/manager"
 	"github.com/kubesphere/kubekey/pkg/util/ssh"
 	"github.com/pkg/errors"
+	"io/ioutil"
 	"os/exec"
 	"strings"
 )
@@ -36,11 +37,11 @@ func DeployNetworkPlugin(mgr *manager.Manager) error {
 	return mgr.RunTaskOnMasterNodes(deployNetworkPlugin, true)
 }
 
-func deployNetworkPlugin(mgr *manager.Manager, node *kubekeyapi.HostCfg, conn ssh.Connection) error {
+func deployNetworkPlugin(mgr *manager.Manager, _ *kubekeyapi.HostCfg, _ ssh.Connection) error {
 	if mgr.Runner.Index == 0 {
 		switch mgr.Cluster.Network.Plugin {
 		case "calico":
-			if err := deployCalico(mgr, node); err != nil {
+			if err := deployCalico(mgr); err != nil {
 				return err
 			}
 		case "flannel":
@@ -48,7 +49,7 @@ func deployNetworkPlugin(mgr *manager.Manager, node *kubekeyapi.HostCfg, conn ss
 				return err
 			}
 		case "macvlan":
-			if err := deployMacvlan(mgr); err != nil {
+			if err := deployMacvlan(); err != nil {
 				return err
 			}
 		default:
@@ -58,31 +59,32 @@ func deployNetworkPlugin(mgr *manager.Manager, node *kubekeyapi.HostCfg, conn ss
 	return nil
 }
 
-func deployCalico(mgr *manager.Manager, node *kubekeyapi.HostCfg) error {
-	var calicoFileBase64 string
-	if util.IsExist(fmt.Sprintf("%s/calico.yaml", mgr.WorkDir)) {
-		output, err := exec.Command("/bin/sh", "-c", fmt.Sprintf("cat %s/calico.yaml | base64 --wrap=0", mgr.WorkDir)).CombinedOutput()
-		if err != nil {
-			fmt.Println(string(output))
-			return errors.Wrap(errors.WithStack(err), fmt.Sprintf("Failed to read custom calico manifests: %s/calico.yaml", mgr.WorkDir))
-		}
-		calicoFileBase64 = strings.TrimSpace(string(output))
-	} else {
-		calicoFile, err := calico.GenerateCalicoFiles(mgr)
+func deployCalico(mgr *manager.Manager) error {
+	if !util.IsExist(fmt.Sprintf("%s/calico.yaml", mgr.WorkDir)) {
+		calicoContent, err := calico.GenerateCalicoFiles(mgr)
 		if err != nil {
 			return err
 		}
-		calicoFileBase64 = base64.StdEncoding.EncodeToString([]byte(calicoFile))
+
+		err1 := ioutil.WriteFile(fmt.Sprintf("%s/calico.yaml", mgr.WorkDir), []byte(calicoContent), 0644)
+		if err1 != nil {
+			return errors.Wrap(errors.WithStack(err1), fmt.Sprintf("Failed to generate calico manifests: %s/calico.yaml", mgr.WorkDir))
+		}
 	}
 
-	_, err1 := mgr.Runner.RunCmd(fmt.Sprintf("sudo -E /bin/sh -c \"echo %s | base64 -d > /etc/kubernetes/calico.yaml\"", calicoFileBase64))
+	calicoBase64, err1 := exec.Command("/bin/bash", "-c", fmt.Sprintf("tar cfz - -C %s -T /dev/stdin <<< calico.yaml | base64 --wrap=0", mgr.WorkDir)).CombinedOutput()
 	if err1 != nil {
-		return errors.Wrap(errors.WithStack(err1), "Failed to generate calico file")
+		return errors.Wrap(errors.WithStack(err1), "Failed to read calico manifests")
 	}
 
-	_, err2 := mgr.Runner.RunCmdOutput("/usr/local/bin/kubectl apply -f /etc/kubernetes/calico.yaml")
+	_, err2 := mgr.Runner.RunCmd(fmt.Sprintf("sudo -E /bin/bash -c \"base64 -d <<< '%s' | tar xz -C %s\"", strings.TrimSpace(string(calicoBase64)), "/etc/kubernetes"))
 	if err2 != nil {
-		return errors.Wrap(errors.WithStack(err2), "Failed to deploy calico")
+		return errors.Wrap(errors.WithStack(err2), "Failed to generate calico manifests")
+	}
+
+	_, err3 := mgr.Runner.RunCmdOutput("/usr/local/bin/kubectl apply -f /etc/kubernetes/calico.yaml")
+	if err3 != nil {
+		return errors.Wrap(errors.WithStack(err3), "Failed to deploy calico")
 	}
 	return nil
 }
@@ -116,6 +118,6 @@ func deployFlannel(mgr *manager.Manager) error {
 	return nil
 }
 
-func deployMacvlan(mgr *manager.Manager) error {
+func deployMacvlan() error {
 	return nil
 }
