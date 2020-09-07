45d1b0c0892716db179d68037e2e8123b733f1e3
zackzhangkai <zackzhang@yunify.com>
2020-09-07 21:28:47 +0800

    add ulimit
    Signed-off-by: zackzhangkai <zackzhang@yunify.com>

--- pkg/cluster/preinstall/tmpl/init_os.go
+++ pkg/cluster/preinstall/tmpl/init_os.go
@@ -109,6 +109,8 @@ update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy >/dev/null 2>&1 |
 update-alternatives --set arptables /usr/sbin/arptables-legacy >/dev/null 2>&1 || true
 update-alternatives --set ebtables /usr/sbin/ebtables-legacy >/dev/null 2>&1 || true
 
+ulimit -u 65535
+ulimit -n 65535
     `)))
 
 func InitOsScript(mgr *manager.Manager) (string, error) {
