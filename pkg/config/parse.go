e28546e610e978d6d4a9f7a90451b29f7aaa9a6b
zgldh <zgldh@hotmail.com>
2020-10-01 03:19:46 +0800

    Signed-off-by: zgldh <zgldh@hotmail.com>

--- pkg/config/parse.go
+++ pkg/config/parse.go
@@ -39,7 +39,7 @@ func ParseClusterCfg(clusterCfgPath, k8sVersion, ksVersion string, ksEnabled boo
 	if len(clusterCfgPath) == 0 {
 		user, _ := user.Current()
 		if user.Username != "root" {
-			return nil, errors.New(fmt.Sprintf("Current user is %s. Please use root!", user.Name))
+			return nil, errors.New(fmt.Sprintf("Current user is %s. Please use root!", user.Username))
 		}
 		clusterCfg = AllinoneCfg(user, k8sVersion, ksVersion, ksEnabled, logger)
 	} else {
