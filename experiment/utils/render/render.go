fbf13cbc622adc80a49e0ad9fee05d2685ad12ac
24sama <jacksama@foxmail.com>
2021-07-30 09:09:54 +0800

    Experiment: abstract load interface
    Signed-off-by: 24sama <jacksama@foxmail.com>

--- experiment/utils/render/render.go
+++ experiment/utils/render/render.go
@@ -2,8 +2,10 @@ package render
 
 import (
 	"fmt"
+	"github.com/pkg/errors"
 	"regexp"
 	"strings"
+	"text/template"
 )
 
 var (
@@ -24,4 +26,33 @@ func Render(input string, replaceVars map[string]string) (string, error) {
 		}
 	}
 	return res, nil
+}
+
+type KubeKeyTemplate struct {
+	Err      error
+	Template *template.Template
+	Buf      *strings.Builder
+}
+
+func NewKebeKeyTemplate(template *template.Template) *KubeKeyTemplate {
+	var buf strings.Builder
+	return &KubeKeyTemplate{
+		Template: template,
+		Buf:      &buf,
+	}
+}
+
+// todo: some bug in buffer
+func (k *KubeKeyTemplate) PauseRender(variables map[string]interface{}) {
+
+	if err := k.Template.Execute(k.Buf, variables); err != nil {
+		k.Err = err
+	}
+}
+
+func (k *KubeKeyTemplate) String() (string, error) {
+	if k.Err != nil {
+		return "", errors.Wrap(k.Err, "Failed to render template")
+	}
+	return k.Buf.String(), nil
 }
