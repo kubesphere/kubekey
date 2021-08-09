26188d4f4354cfa5393e0ba9176fafcfdc5ddf8d
24sama <jacksama@foxmail.com>
2021-08-09 12:50:20 +0800

    Experiment: fix some logger bug
    Signed-off-by: 24sama <jacksama@foxmail.com>

--- experiment/utils/config/manager.go
+++ experiment/utils/config/manager.go
@@ -13,7 +13,7 @@ import (
 type Manager struct {
 	ObjName            string
 	Cluster            *kubekeyapiv1alpha1.ClusterSpec
-	Logger             *logger.KubeKeyLog
+	Logger             logger.KubeKeyLog
 	Connector          connector.Connector
 	Runner             *runner.Runner
 	AllNodes           []kubekeyapiv1alpha1.HostCfg
