9fcb657da57022f4e10139be17c0be8494974f08
24sama <jacksama@foxmail.com>
2021-08-09 12:50:20 +0800

    Experiment: fix some logger bug
    Signed-off-by: 24sama <jacksama@foxmail.com>

--- experiment/utils/logger/logger_test.go
+++ experiment/utils/logger/logger_test.go
@@ -10,14 +10,16 @@ func TestKubeKey_Print(t *testing.T) {
 	log := NewLogger()
 	wg := &sync.WaitGroup{}
 	for i := 0; i < 20; i++ {
+		log.SetModule("CREATE")
+		log.SetTask("task1")
+		log.SetNode("node" + strconv.Itoa(i))
+		l1 := *log
 		wg.Add(1)
-		go func(x int) {
-			log.SetModule("CREATE")
-			log.SetTask("task1")
-			log.SetNode("node" + strconv.Itoa(x))
+		go func(x int, log KubeKeyLog) {
+
 			log.Info("Congratulations!", "ssssss")
 			wg.Done()
-		}(i)
+		}(i, l1)
 	}
 	wg.Wait()
 }
