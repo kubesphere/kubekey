a016e025b80c141d3bc95659054feeeaf3df01b8
sbhnet <sbhnet@163.com>
2021-09-30 09:43:43 +0800

    Update cluster_controller.go

--- controllers/kubekey/cluster_controller.go
+++ controllers/kubekey/cluster_controller.go
@@ -275,7 +275,7 @@ func ignoreDeletionPredicate() predicate.Predicate {
 	return predicate.Funcs{
 		UpdateFunc: func(e event.UpdateEvent) bool {
 			// Ignore updates to CR status in which case metadata.Generation does not change
-			return e.MetaOld.GetGeneration() != e.MetaNew.GetGeneration()
+			return e.ObjectOld.GetGeneration() != e.ObjectNew.GetGeneration()
 		},
 		DeleteFunc: func(e event.DeleteEvent) bool {
 			// Evaluates to false if the object has been confirmed deleted.
