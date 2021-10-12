cd43c0918288c1b71d5499a1088262587b1f06f1
24sama <leo@kubesphere.io>
2021-10-12 13:58:50 +0800

    dev-v2.0.0: add DeployLocalVolumeModule
    Signed-off-by: 24sama <leo@kubesphere.io>

---
+++ pkg/pipelines/plugins/storage/tasks.go
@@ -0,0 +1,21 @@
+package storage
+
+import (
+	"fmt"
+	"github.com/kubesphere/kubekey/pkg/core/connector"
+	"github.com/kubesphere/kubekey/pkg/pipelines/common"
+	"github.com/pkg/errors"
+	"path/filepath"
+)
+
+type DeployLocalVolume struct {
+	common.KubeAction
+}
+
+func (d *DeployLocalVolume) Execute(runtime connector.Runtime) error {
+	cmd := fmt.Sprintf("/usr/local/bin/kubectl apply -f %s", filepath.Join(common.KubeAddonsDir, "local-volume.yaml"))
+	if _, err := runtime.GetRunner().SudoCmd(cmd, false); err != nil {
+		return errors.Wrap(errors.WithStack(err), "deploy local-volume.yaml failed")
+	}
+	return nil
+}
