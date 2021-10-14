f4676930d09684ab54a925953355b5d22763c9ac
24sama <leo@kubesphere.io>
2021-10-14 16:50:46 +0800

    dev-v2.0.0: add KubeSphereModule
    Signed-off-by: 24sama <leo@kubesphere.io>

---
+++ pkg/pipelines/version/kubesphere/templates/cc_v310.go
@@ -0,0 +1,124 @@
+package templates
+
+import (
+	"github.com/lithammer/dedent"
+	"text/template"
+)
+
+var V310 = template.Must(template.New("v3.1.0").Parse(
+	dedent.Dedent(`---
+apiVersion: installer.kubesphere.io/v1alpha1
+kind: ClusterConfiguration
+metadata:
+  name: ks-installer
+  namespace: kubesphere-system
+  labels:
+    version: {{ .Tag }}
+spec:
+  persistence:
+    storageClass: ""       
+  authentication:
+    jwtSecret: ""
+  zone: ""
+  local_registry: ""        
+  etcd:
+    monitoring: false      
+    endpointIps: localhost  
+    port: 2379             
+    tlsEnable: true
+  common:
+    redis:
+      enabled: false
+    redisVolumSize: 2Gi 
+    openldap:
+      enabled: false
+    openldapVolumeSize: 2Gi  
+    minioVolumeSize: 20Gi
+    monitoring:
+      endpoint: http://prometheus-operated.kubesphere-monitoring-system.svc:9090
+    es:  
+      elasticsearchMasterVolumeSize: 4Gi   
+      elasticsearchDataVolumeSize: 20Gi   
+      logMaxAge: 7          
+      elkPrefix: logstash
+      basicAuth:
+        enabled: false
+        username: ""
+        password: ""
+      externalElasticsearchUrl: ""
+      externalElasticsearchPort: ""  
+  console:
+    enableMultiLogin: true 
+    port: 30880
+  alerting:       
+    enabled: false
+    # thanosruler:
+    #   replicas: 1
+    #   resources: {}
+  auditing:    
+    enabled: false
+  devops:           
+    enabled: false
+    jenkinsMemoryLim: 2Gi     
+    jenkinsMemoryReq: 1500Mi 
+    jenkinsVolumeSize: 8Gi   
+    jenkinsJavaOpts_Xms: 512m  
+    jenkinsJavaOpts_Xmx: 512m
+    jenkinsJavaOpts_MaxRAM: 2g
+  events:          
+    enabled: false
+    ruler:
+      enabled: true
+      replicas: 2
+  logging:         
+    enabled: false
+    logsidecar:
+      enabled: true
+      replicas: 2
+  metrics_server:             
+    enabled: false
+  monitoring:
+    storageClass: ""
+    prometheusMemoryRequest: 400Mi  
+    prometheusVolumeSize: 20Gi  
+  multicluster:
+    clusterRole: none 
+  network:
+    networkpolicy:
+      enabled: false
+    ippool:
+      type: none
+    topology:
+      type: none
+  openpitrix:
+    store:
+      enabled: false
+  servicemesh:    
+    enabled: false  
+  kubeedge:
+    enabled: false
+    cloudCore:
+      nodeSelector: {"node-role.kubernetes.io/worker": ""}
+      tolerations: []
+      cloudhubPort: "10000"
+      cloudhubQuicPort: "10001"
+      cloudhubHttpsPort: "10002"
+      cloudstreamPort: "10003"
+      tunnelPort: "10004"
+      cloudHub:
+        advertiseAddress: 
+          - ""           
+        nodeLimit: "100"
+      service:
+        cloudhubNodePort: "30000"
+        cloudhubQuicNodePort: "30001"
+        cloudhubHttpsNodePort: "30002"
+        cloudstreamNodePort: "30003"
+        tunnelNodePort: "30004"
+    edgeWatcher:
+      nodeSelector: {"node-role.kubernetes.io/worker": ""}
+      tolerations: []
+      edgeWatcherAgent:
+        nodeSelector: {"node-role.kubernetes.io/worker": ""}
+        tolerations: []
+`)))
