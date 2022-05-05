16d24f76f9e680a5363de9c7732632ebee4c8678
hellocn9 <zhangyancn9@126.com>
2022-05-05 16:43:53 +0800

    fix: remove plainHTTP

--- pkg/images/tasks.go
+++ pkg/images/tasks.go
@@ -332,7 +332,7 @@ func (p *PushManifest) Execute(_ connector.Runtime) error {
 		logger.Log.Infof("Push multi-arch manifest list: %s", imageName)
 		// todo: the function can't support specify a certs dir
 		digest, length, err := manifestregistry.PushManifestList(auth.Username, auth.Password, manifestSpec,
-			false, true, p.KubeConf.Cluster.Registry.PlainHTTP, "")
+			false, true, auth.SkipTLSVerify, "")
 		if err != nil {
 			return errors.Wrap(errors.WithStack(err), fmt.Sprintf("push image %s multi-arch manifest failed", imageName))
 		}
