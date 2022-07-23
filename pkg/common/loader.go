97ad9f358074647a1b21d7d23403b85cf70b7949
失眠是真滴难受 <imbozhong@gmail.com>
2022-07-23 10:24:58 +0800

    feat: support version without "v" (#1396)
    support kubenetes and kubesphere version without "v"

--- pkg/common/loader.go
+++ pkg/common/loader.go
@@ -24,6 +24,7 @@ import (
 	"os/exec"
 	"os/user"
 	"path/filepath"
+	"regexp"
 	"runtime"
 	"strings"
 	"time"
@@ -35,6 +36,10 @@ import (
 	kubekeyapiv1alpha2 "github.com/kubesphere/kubekey/apis/kubekey/v1alpha2"
 	"github.com/kubesphere/kubekey/pkg/core/util"
 	"github.com/kubesphere/kubekey/pkg/version/kubesphere"
+)
+
+var (
+	kubeReleaseRegex = regexp.MustCompile(`^v?(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)([-0-9a-zA-Z_\.+]*)?$`)
 )
 
 type Loader interface {
@@ -108,8 +113,8 @@ func (d *DefaultLoader) Load() (*kubekeyapiv1alpha2.Cluster, error) {
 		Worker:   {hostname},
 		Registry: {hostname},
 	}
-	if d.KubernetesVersion != "" {
-		s := strings.Split(d.KubernetesVersion, "-")
+	if ver := normalizedBuildVersion(d.KubernetesVersion); ver != "" {
+		s := strings.Split(ver, "-")
 		if len(s) > 1 {
 			allInOne.Spec.Kubernetes = kubekeyapiv1alpha2.Kubernetes{
 				Version: s[0],
@@ -117,7 +122,7 @@ func (d *DefaultLoader) Load() (*kubekeyapiv1alpha2.Cluster, error) {
 			}
 		} else {
 			allInOne.Spec.Kubernetes = kubekeyapiv1alpha2.Kubernetes{
-				Version: d.KubernetesVersion,
+				Version: ver,
 			}
 		}
 	} else {
@@ -127,7 +132,11 @@ func (d *DefaultLoader) Load() (*kubekeyapiv1alpha2.Cluster, error) {
 	}
 
 	if d.KubeSphereEnable {
-		if err := defaultKSConfig(&allInOne.Spec.KubeSphere, d.KubeSphereVersion); err != nil {
+		ver := normalizedBuildVersion(d.KubeSphereVersion)
+		if ver == "" {
+			return nil, errors.New(fmt.Sprintf("Unsupported Kubesphere Version: %v\n", d.KubeSphereVersion))
+		}
+		if err := defaultKSConfig(&allInOne.Spec.KubeSphere, ver); err != nil {
 			return nil, err
 		}
 	}
@@ -230,18 +239,22 @@ func (f FileLoader) Load() (*kubekeyapiv1alpha2.Cluster, error) {
 	}
 
 	if f.KubeSphereEnable {
-		if err := defaultKSConfig(&clusterCfg.Spec.KubeSphere, f.KubeSphereVersion); err != nil {
+		ver := normalizedBuildVersion(f.KubeSphereVersion)
+		if ver == "" {
+			return nil, errors.New(fmt.Sprintf("Unsupported Kubesphere Version: %v\n", f.KubeSphereVersion))
+		}
+		if err := defaultKSConfig(&clusterCfg.Spec.KubeSphere, ver); err != nil {
 			return nil, err
 		}
 	}
 
-	if f.KubernetesVersion != "" {
-		s := strings.Split(f.KubernetesVersion, "-")
+	if ver := normalizedBuildVersion(f.KubernetesVersion); ver != "" {
+		s := strings.Split(ver, "-")
 		if len(s) > 1 {
 			clusterCfg.Spec.Kubernetes.Version = s[0]
 			clusterCfg.Spec.Kubernetes.Type = s[1]
 		} else {
-			clusterCfg.Spec.Kubernetes.Version = f.KubernetesVersion
+			clusterCfg.Spec.Kubernetes.Version = ver
 		}
 	}
 
@@ -249,6 +262,8 @@ func (f FileLoader) Load() (*kubekeyapiv1alpha2.Cluster, error) {
 		clusterCfg.Spec.Kubernetes.ContainerManager = f.arg.ContainerManager
 	}
 
+	clusterCfg.Spec.Kubernetes.Version = normalizedBuildVersion(clusterCfg.Spec.Kubernetes.Version)
+	clusterCfg.Spec.KubeSphere.Version = normalizedBuildVersion(clusterCfg.Spec.KubeSphere.Version)
 	clusterCfg.Name = objName
 	return &clusterCfg, nil
 }
@@ -277,4 +292,16 @@ func defaultKSConfig(ks *kubekeyapiv1alpha2.KubeSphere, version string) error {
 		return errors.New(fmt.Sprintf("Unsupported KubeSphere version: %s", version))
 	}
 	return nil
+}
+
+// normalizedBuildVersion used to returns normalized build version (with "v" prefix if needed)
+// If input doesn't match known version pattern, returns empty string.
+func normalizedBuildVersion(version string) string {
+	if kubeReleaseRegex.MatchString(version) {
+		if strings.HasPrefix(version, "v") {
+			return version
+		}
+		return "v" + version
+	}
+	return ""
 }
