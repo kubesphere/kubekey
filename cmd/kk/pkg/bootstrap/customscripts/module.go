8d290ebefb86e1c982e74e50a5b74ae1951d12ea
zaunist <y.bz@foxmail.com>
2023-02-20 06:20:12 +0000

    fix: copy kubeconfig  to .kube dir

--- cmd/kk/pkg/bootstrap/customscripts/module.go
+++ cmd/kk/pkg/bootstrap/customscripts/module.go
@@ -49,4 +49,24 @@ func (m *CustomScriptsModule) Init() {
 
 		m.Tasks = append(m.Tasks, task)
 	}
+
+	m.CopyKubeConfig()
+}
+
+func (m *CustomScriptsModule) CopyKubeConfig() {
+	taskDir := fmt.Sprintf("%s-default-script", m.Phase)
+	taskName := fmt.Sprintf("Phase:%s CopyKubeConfig", m.Phase)
+	script := kubekeyapiv1alpha2.CustomScripts{
+		Name: "CopyKubeConfig",
+		Bash: "cp /etc/kubernetes/admin.conf $HOME/.kube/config",
+	}
+
+	task := task.RemoteTask{
+		Name:   taskName,
+		Desc:   taskName,
+		Hosts:  m.Runtime.GetAllHosts(),
+		Action: &CustomScriptTask{taskDir: taskDir, script: script},
+	}
+
+	m.Tasks = append(m.Tasks, &task)
 }
