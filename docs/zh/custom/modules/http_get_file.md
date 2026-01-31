d66a535d642893130ea40490fdd137ce1e4a8348
mumuhhh <14306508+mumuhhh@users.noreply.github.com>
2026-01-31 10:42:06 +0800

    feat: 支持直接下载http文件
    Signed-off-by: mumuhhh <14306508+mumuhhh@users.noreply.github.com>

---
+++ docs/zh/custom/modules/http_get_file.md
@@ -0,0 +1,33 @@
+# http_get_file 模块
+
+从http文件服务拉取文件到本地。
+
+## 参数
+
+| 参数   | 说明 | 类型 | 必填 | 默认值 |
+|------|------|------|------|--------|
+| url  | http文件服务上的文件路径 | 字符串 | 是 | - |
+| dest | 本地保存路径 | 字符串 | 是 | - |
+| username | Basic 认证用户名 | 字符串 | 否 | - |
+| password | Basic 认证密码 | 字符串 | 否 | - |
+| token | Bearer Token | 字符串 | 否 | - |
+| timeout | 请求超时 | 字符串 | 否 | 10s |
+| headers | 自定义 HTTP 头 | map | 否 | - |
+
+## 示例
+
+**1. 拉取文件**
+
+```yaml
+- name: http fetch file
+  when: '{{ "/tmp/test.tar.gz" | fileExist | not }}'
+  http_get_file:
+    url: http://localhost/test.tar.gz     # http文件服务上的文件路径
+    dest: /tmp/test.tar.gz                # 本地保存路径
+    username: admin                       # 可选：Basic 认证用户名
+    password: password                    # 可选：Basic 认证密码
+    token: my-token                       # 可选：Bearer Token
+    timeout: 10s                          # 可选：请求超时，默认 10s
+    headers:                              # 可选：自定义 HTTP 头
+      X-Custom-Header: custom-value
+```
---
+++ pkg/modules/http_get_file.go
@@ -0,0 +1,242 @@
+/*
+Copyright 2023 The KubeSphere Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package modules
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/cockroachdb/errors"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/klog/v2"
+
+	_const "github.com/kubesphere/kubekey/v4/pkg/const"
+	"github.com/kubesphere/kubekey/v4/pkg/variable"
+)
+
+const (
+	// Default timeout for http API
+	defaultHttpTimeout = 10 * time.Second
+)
+
+type httpArgs struct {
+	url      string
+	username string
+	password string
+	token    string
+	headers  map[string]string
+	timeout  time.Duration
+	client   *http.Client
+}
+
+func (hc *httpArgs) Init(ctx context.Context) error {
+	// Ensure URL is provided
+	if hc.url == "" {
+		return errors.New("http URL is required")
+	}
+
+	// Parse and normalize the URL
+	parsedURL, err := url.Parse(hc.url)
+	if err != nil {
+		return errors.Wrapf(err, "invalid http URL: %s", hc.url)
+	}
+
+	// Default to http if scheme is missing
+	if parsedURL.Scheme == "" {
+		klog.V(4).InfoS("No scheme specified in http URL, defaulting to HTTP", "url", hc.url)
+		parsedURL.Scheme = "http"
+	} else if parsedURL.Scheme != "http" && parsedURL.Scheme != "https" {
+		return errors.Errorf("unsupported URL scheme: %s, only http and https are supported", parsedURL.Scheme)
+	}
+
+	// Update the URL with normalized version
+	hc.url = parsedURL.String()
+	klog.V(4).InfoS("Initializing http connector", "url", hc.url)
+
+	// Create HTTP client with timeout
+	hc.client = &http.Client{
+		Timeout: hc.timeout,
+	}
+	return nil
+}
+
+// FetchFile from http file server.  dst is the local writer.
+func (hc *httpArgs) FetchFile(ctx context.Context, dst io.Writer) error {
+	// Create request
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, hc.url, http.NoBody)
+	if err != nil {
+		return errors.Wrap(err, "failed to create request for server info")
+	}
+
+	// Add authentication headers
+	hc.addAuthHeaders(req)
+
+	// Execute request
+	resp, err := hc.client.Do(req)
+	if err != nil {
+		return errors.Wrap(err, "failed to get http server info")
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusOK {
+		if dst != nil {
+			_, err = io.Copy(dst, resp.Body)
+		}
+		return err
+	}
+	bodyBytes, _ := io.ReadAll(resp.Body)
+	klog.ErrorS(err, "http server info request failed",
+		"statusCode", resp.StatusCode,
+		"response", string(bodyBytes))
+	return errors.Errorf("http server info request failed with status %d", resp.StatusCode)
+}
+
+// addAuthHeaders adds authentication headers to the request
+func (hc *httpArgs) addAuthHeaders(req *http.Request) {
+	// Add basic auth if username and password are provided
+	if hc.username != "" && hc.password != "" {
+		req.SetBasicAuth(hc.username, hc.password)
+		klog.V(4).InfoS("Added basic auth header to request", "username", hc.username)
+	}
+
+	// Add token auth if token is provided
+	if hc.token != "" {
+		req.Header.Set("Authorization", "Bearer "+hc.token)
+		klog.V(4).InfoS("Added token auth header to request")
+	}
+
+	// Add custom headers
+	for k, v := range hc.headers {
+		req.Header.Set(k, v)
+		klog.V(4).InfoS("Added custom header to request", "key", k)
+	}
+}
+
+func newHttpArgs(ctx context.Context, args map[string]any, vars map[string]any) (httpArg *httpArgs, err error) {
+
+	httpArg = &httpArgs{
+		headers: make(map[string]string),
+		timeout: defaultHttpTimeout,
+	}
+
+	// Retrieve http URL
+	httpURL, err := variable.StringVar(vars, args, "url")
+	if err != nil {
+		klog.V(4).InfoS("Failed to get http url, using current url", "error", err)
+	}
+	httpArg.url = httpURL
+
+	// Retrieve username
+	username, err := variable.StringVar(vars, args, _const.VariableConnectorUserName)
+	if err != nil {
+		klog.V(4).InfoS("Failed to get http username, using current username", "error", err)
+	}
+	httpArg.username = username
+
+	// Retrieve password
+	password, err := variable.StringVar(vars, args, _const.VariableConnectorPassword)
+	if err != nil {
+		klog.V(4).InfoS("Failed to get http password, using current password", "error", err)
+	}
+	httpArg.password = password
+
+	// Retrieve token
+	token, err := variable.StringVar(vars, args, _const.VariableConnectorToken)
+	if err != nil {
+		klog.V(4).InfoS("Failed to get connector token, using current token", "error", err)
+	}
+	httpArg.token = token
+
+	if headers, ok := args["headers"].(map[string]any); ok {
+		for k, v := range headers {
+			if strVal, ok := v.(string); ok {
+				httpArg.headers[k] = strVal
+			}
+		}
+	}
+	if timeoutStr, ok := args["timeout"].(string); ok {
+		if timeout, err := time.ParseDuration(timeoutStr); err == nil {
+			httpArg.timeout = timeout
+		}
+	}
+	return httpArg, httpArg.Init(ctx)
+}
+
+func ModuleHttpGetFile(ctx context.Context, options ExecOptions) (string, string, error) {
+	// get host variable
+	ha, err := options.getAllVariables()
+	if err != nil {
+		return StdoutFailed, StderrGetHostVariable, err
+	}
+
+	// check args
+	args := variable.Extension2Variables(options.Args)
+	// Parse module arguments.
+	httpArg, err := newHttpArgs(ctx, args, ha)
+	if err != nil {
+		return StdoutFailed, StderrParseArgument, err
+	}
+
+	destParam, err := variable.StringVar(ha, args, "dest")
+	if err != nil {
+		return StdoutFailed, "\"dest\" in args should be string", err
+	}
+
+	// fetch file
+	parentDir := filepath.Dir(destParam)
+	if _, err := os.Stat(parentDir); os.IsNotExist(err) {
+		if err := os.MkdirAll(parentDir, os.ModePerm); err != nil {
+			return StdoutFailed, "failed to create dest dir", err
+		}
+	}
+
+	tmpFile, err := os.CreateTemp(parentDir, "*.tmp")
+	if err != nil {
+		return StdoutFailed, "failed to create dest file", err
+	}
+
+	tmpFilePath := tmpFile.Name()
+	defer func() {
+		_ = os.Remove(tmpFilePath)
+	}()
+
+	err = httpArg.FetchFile(ctx, tmpFile)
+	errC := tmpFile.Close()
+	if err != nil {
+		return StdoutFailed, "failed to get http file", err
+	}
+	if errC != nil {
+		return StdoutFailed, "failed to close tmp file", errC
+	}
+
+	err = os.Rename(tmpFilePath, destParam)
+	if err != nil {
+		return StdoutFailed, "failed to rename file", err
+	}
+
+	return StdoutSuccess, "", nil
+}
+
+func init() {
+	// Register httpGetFile module
+	utilruntime.Must(RegisterModule("http_get_file", ModuleHttpGetFile))
+}
